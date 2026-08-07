a2df924d01f6bb07e7ac6f0e7a0b39b618a393c9
Padmaja-Dhulipudi <padmajadulipudi@gmail.com>
2026-08-07 11:29:44 +0530

    Complete BloodLink frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/donor", donorRoutes);
@@ -24,6 +24,8 @@ app.use("/api/request", requestRoutes);
 
 app.use("/api/dashboard", dashboardRoutes);
 
+app.use(errorHandler);
+
 app.get("/api/profile", protect, (req, res) => {
     res.json({
         success: true,
@@ -32,7 +34,7 @@ app.get("/api/profile", protect, (req, res) => {
     });
 });
 
-// Test Route
+
 app.get("/", (req, res) => {
     res.send("BloodLink Backend Running...");
 });
@@ -42,4 +44,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-app.use(errorHandler);
